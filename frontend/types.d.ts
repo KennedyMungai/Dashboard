56f6a1bad30f378816510e155781ab23d19ebc42
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 16:31:22 +0300

    The base of the types data file

---
+++ frontend/types.d.ts
@@ -0,0 +1,59 @@
+interface ISupermarketSales
+{
+    data: {
+        total_sales:total_sales_per_branch,
+        sales_by_gender:sales_by_gender,
+        gross_income_data:gross_income_data,
+        product_line_by_total_sales:product_line_by_total_sales,
+        product_line_by_gross_income:product_line_by_gross_income,
+        product_line_by_rating:product_line_by_rating,
+        payment_methods:payment_methods,
+        product_line_by_quantity:product_line_by_quantity,
+        shopping_hour_data: shopping_hour_data
+    }
+}
+
+interface total_sales_per_branch
+{
+
+}
+
+interface sales_by_gender
+{
+
+}
+
+interface gross_income_data
+{
+
+}
+
+interface product_line_by_total_sales
+{
+
+}
+
+interface product_line_by_gross_income
+{
+
+}
+
+interface product_line_by_rating
+{
+
+}
+
+interface payment_methods
+{
+
+}
+
+interface product_line_by_quantity
+{
+
+}
+
+interface shopping_hour_data
+{
+    
+}
