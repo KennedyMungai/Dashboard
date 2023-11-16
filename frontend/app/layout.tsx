2c932529bf63f4b4985523568a0f0f7d6e735908
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 14:59:55 +0300

    Initial setup of the project

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+import './globals.css'
+
+const open_sans = Open_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'Simple Dashboard app with Charts',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={open_sans.className}>{children}</body>
+    </html>
+  )
+}
