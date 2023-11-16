3967e27e5d8399e4c4ea52dbba98b5b50183bb35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 15:57:07 +0300

    Created the ThemeToggle component

---
+++ frontend/components/NavBar/ThemeToggle.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+}
+
+const ThemeProvider = ({ children }: Props) => {
+	;<NextThemesProvider attribute='class' enableSystem defaultTheme='system'>
+		{children}
+	</NextThemesProvider>
+}
