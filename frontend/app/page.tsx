1ec8b86b47ac2947cfd117f03375a53989bf100f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 15:54:17 +0300

    Deleted the title of the page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="pt-5">
-      <h1 className="w-full flex justify-center uppercase text-2xl">Something Something Charts App</h1>
+      
     </main>
   )
 }
