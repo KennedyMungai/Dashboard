77b932ea79157282bcc7f8e705ce32a7a008522a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 16:20:54 +0300

    Added the Redux provider

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={open_sans.className}>
-				<ThemeProvider><NavBar />{children}</ThemeProvider>
+				<ThemeProvider>
+					<NavBar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	)
