0136436157905c8eac4c84bdc392ba5de648040d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 16:08:22 +0300

    Added the ThemeToggle component

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import ThemeProvider from '../../providers/ThemeProvider'
+import ThemeToggle from './ThemeToggle'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
-  return (
-    <nav className='h-[10vh] shadow-sm dark:shadow-slate-700'>
-        <div className=""><h1 className="w-full flex justify-center uppercase text-2xl">Something Something Charts App</h1></div>
-        <div className=""><ThemeProvider /></div>
-    </nav>
-  )
+	return (
+		<nav className='h-[10vh] shadow-sm dark:shadow-slate-700 flex justify-between'>
+			<div className=''>
+				<h1 className='w-full flex justify-center uppercase text-2xl'>
+					Something Something Charts App
+				</h1>
+			</div>
+			<div className=''>
+				<ThemeToggle />
+			</div>
+		</nav>
+	)
 }
 
 export default NavBar
