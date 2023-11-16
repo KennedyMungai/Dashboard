991ce2057cb779b43d66f2c6617a7c9d34449bc6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 16:02:05 +0300

    Made some updates

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ThemeToggle from './ThemeToggle'
+import ThemeProvider from '../../providers/ThemeProvider'
 
 type Props = {}
 
@@ -7,7 +7,7 @@ const NavBar = (props: Props) => {
   return (
     <nav className='h-[10vh] shadow-sm dark:shadow-slate-700'>
         <div className=""><h1 className="w-full flex justify-center uppercase text-2xl">Something Something Charts App</h1></div>
-        <div className=""><ThemeToggle /></div>
+        <div className=""><ThemeProvider /></div>
     </nav>
   )
 }
