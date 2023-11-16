93aeaa195e8c0b67061c63d79b7217d1c6297a8e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-16 15:54:03 +0300

    Created the Navbar component

---
+++ frontend/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const NavBar = (props: Props) => {
+  return (
+    <nav className='h-[10vh] shadow-sm dark:shadow-slate-700'>
+        <div className=""><h1 className="w-full flex justify-center uppercase text-2xl">Something Something Charts App</h1></div>
+        <div className=""><ThemeToggle /></div>
+    </nav>
+  )
+}
+
+export default NavBar
