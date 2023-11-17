daf4169603f07480c20f801905154da68ea0d114
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 10:58:51 +0300

    Added all the Charts to the ChartGrid

--- frontend/components/Charts/ChartGrid.tsx
+++ frontend/components/Charts/ChartGrid.tsx
@@ -1,12 +1,52 @@
 'use client'
 
+import { Line } from 'react-chartjs-2'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import { useAppSelector } from '@/redux/hooks'
-import GrossIncome from './GrossIncome'
+import { selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
 
 type Props = {}
 
 const ChartGrid = (props: Props) => {
-	return <div className='w-full h-full'><GrossIncome /></div>
+    const salesData = useAppSelector(selectSupermarketSalesData)
+
+    const data = salesData.gross_income_data
+
+	return <div className='w-full h-full p-5 flex flex-1 flex-wrap'>
+        <div className="">
+        <Card className='w-[5rem]'>
+			<CardHeader>
+				<CardTitle>Line Chart</CardTitle>
+				<CardDescription>
+					Line Chart Data for the supermarket data
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Line 
+                    datasetIdKey='id' 
+                    data={{
+                        labels: data.labels,
+                        datasets: [{
+                            label: data.label,
+                            data: data.data,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    }} 
+                />
+			</CardContent>
+		</Card>
+        </div>
+    </div>
 }
 
 export default ChartGrid
