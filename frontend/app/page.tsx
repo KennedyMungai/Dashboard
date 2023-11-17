9902df71845091a67580caf31461e9345137d5d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 10:26:26 +0300

    Made some changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { getSupermarketSales, selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const dispatch = useAppDispatch()
   const supermarketData = useAppSelector(selectSupermarketSalesData)
+
+  const [supermarketSalesData, setSupermarketSalesData] = useState<ISupermarketSales>()
 
   useEffect(() => {
     dispatch(getSupermarketSales())
