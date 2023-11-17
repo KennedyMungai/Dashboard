5836599d3b675e4364a892c16b4bd57dbb9d63c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 09:55:31 +0300

    Rendered the MainPage component on the index page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import MainPage from '@/components/MainPage/MainPage'
+
 export default function Home() {
-  return (
-    <main className="pt-5">
-      
-    </main>
-  )
+	return (
+		<main className='pt-5'>
+			<MainPage />
+		</main>
+	)
 }
