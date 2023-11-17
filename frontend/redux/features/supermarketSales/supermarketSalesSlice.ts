48396dbeefc5db3b870edaadd1feebaddc39e387
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 09:25:59 +0300

    Added the intial state of the data

--- frontend/redux/features/supermarketSales/supermarketSalesSlice.ts
+++ frontend/redux/features/supermarketSales/supermarketSalesSlice.ts
@@ -1,5 +1,19 @@
 import {
-    createSlice, 
-    PayloadAction, 
+    createSlice,
+    PayloadAction,
     createAsyncThunk
 } from "@reduxjs/toolkit"
+
+const initialState: ISupermarketSales = {
+    data: {
+        total_sales_per_branch: { labels: [], data: [], label: '' },
+        sales_by_gender: { labels: [], data: [], label: '' },
+        gross_income_data: { labels: [], data: [], label: '' },
+        product_line_by_total_sales: { labels: [], data: [], label: '' },
+        product_line_by_gross_income: { labels: [], data: [], label: '' },
+        product_line_by_rating: { labels: [], data: [], label: '' },
+        payment_methods: { labels: [], data: [], label: '' },
+        product_line_by_quantity: { labels: [], data: [], label: '' },
+        shopping_hour_data: { labels: [], label: '', A: { label: '', data: [] }, B: { label: '', data: [] }, C: { label: '', data: [] } }
+    }
+}
