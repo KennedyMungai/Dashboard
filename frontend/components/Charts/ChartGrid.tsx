20ad2916b38e6c9cbe718608ad76bc2ab3e88ecc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:02:55 +0300

    Made some changes to the component

--- frontend/components/Charts/ChartGrid.tsx
+++ frontend/components/Charts/ChartGrid.tsx
@@ -4,8 +4,11 @@ import { Line } from 'react-chartjs-2'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import { useAppSelector } from '@/redux/hooks'
 import { selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from 'chart.js'
 
 type Props = {}
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
 const ChartGrid = (props: Props) => {
     const salesData = useAppSelector(selectSupermarketSalesData)
@@ -14,14 +17,14 @@ const ChartGrid = (props: Props) => {
 
 	return <div className='w-full h-full p-5 flex flex-1 flex-wrap'>
         <div className="">
-        <Card className='w-[5rem]'>
+        <Card className='w-[20rem]'>
 			<CardHeader>
 				<CardTitle>Line Chart</CardTitle>
 				<CardDescription>
 					Line Chart Data for the supermarket data
 				</CardDescription>
 			</CardHeader>
-			<CardContent>
+			<CardContent className='flex flex-1'>
 				<Line 
                     datasetIdKey='id' 
                     data={{
