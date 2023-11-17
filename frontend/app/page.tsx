7d57ed8919c6c0c22e96a6785b0e18d634902056
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 10:10:26 +0300

    Set up redux for data fetching

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,21 @@
-import MainPage from '@/components/MainPage/MainPage'
+'use client'
+import { getSupermarketSales, selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const dispatch = useAppDispatch()
+  const supermarketData = useAppSelector(selectSupermarketSalesData)
+
+  useEffect(() => {
+    dispatch(getSupermarketSales())
+  }, [])
+  
+  console.log(supermarketData)
+
 	return (
 		<main className='pt-5'>
-			<MainPage />
+			{/* <MainPage /> */}
 		</main>
 	)
 }
