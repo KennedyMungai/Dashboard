7804ee012531d401fa81d99815eb333eddcd6495
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 09:18:57 +0300

    Implemented some types for the project

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,7 +1,7 @@
 interface ISupermarketSales
 {
     data: {
-        total_sales:total_sales_per_branch,
+        total_sales_per_branch:total_sales_per_branch,
         sales_by_gender:sales_by_gender,
         gross_income_data:gross_income_data,
         product_line_by_total_sales:product_line_by_total_sales,
@@ -15,45 +15,82 @@ interface ISupermarketSales
 
 interface total_sales_per_branch
 {
-
+    labels: string[]
+    data: number
+    label: string
 }
 
 interface sales_by_gender
 {
-
+    labels: string[]
+    data: number
+    label: string
 }
 
 interface gross_income_data
 {
-
+    labels: string[]
+    data: number
+    label: string
 }
 
 interface product_line_by_total_sales
 {
-
+    labels: string[]
+    data: number
+    label: string
 }
 
 interface product_line_by_gross_income
 {
-
+labels: string[]
+    data: number
+    label: string
 }
 
 interface product_line_by_rating
 {
-
+labels: string[]
+    data: number
+    label: string
 }
 
 interface payment_methods
 {
-
+labels: string[]
+    data: number
+    label: string
 }
 
 interface product_line_by_quantity
 {
-
+labels: string[]
+    data: number
+    label: string
 }
 
 interface shopping_hour_data
 {
-    
+    labels: string[]
+    A:A
+    B:B
+    C:C
+}
+
+interface A
+{
+    label: string
+    data: number[]
+}
+
+interface B
+{
+    label: string
+    data: number[]
+}
+
+interface C
+{
+    label: string
+    data: number[]
 }
