f209a1916294c4870796223a9d6f193dc33a05dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 09:54:25 +0300

    IMplemented redux in the MainPage component

--- frontend/components/MainPage/MainPage.tsx
+++ frontend/components/MainPage/MainPage.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import { selectSupermarketSalesData } from "@/redux/features/supermarketSales/supermarketSalesSlice"
+import { useAppDispatch, useAppSelector } from "@/redux/hooks"
+
 type Props = {}
 
 const MainPage = (props: Props) => {
+    const dispatch = useAppDispatch()
+    const supermarketData = useAppSelector(selectSupermarketSalesData)
+
   return (
     <div>MainPage</div>
   )
