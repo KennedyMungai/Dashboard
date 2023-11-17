d4b6808fa8cb3355f1768d4a6025e4ebb55a03a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 10:35:18 +0300

    Removed the state management code:

--- frontend/components/Charts/ChartGrid.tsx
+++ frontend/components/Charts/ChartGrid.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
 import { useAppSelector } from '@/redux/hooks'
 
 type Props = {}
 
 const ChartGrid = (props: Props) => {
-	const salesData = useAppSelector(selectSupermarketSalesData)
-
 	return <div className='w-full h-full'>ChartGrid</div>
 }
 
