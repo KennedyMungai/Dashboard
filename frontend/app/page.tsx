7985679fd26f479e26fd14eafcb652e22d508755
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 10:31:16 +0300

    Properly formattedthe component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,19 @@
 'use client'
 import ChartGrid from '@/components/Charts/ChartGrid'
-import { getSupermarketSales, selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { useEffect, useState } from 'react'
+import { getSupermarketSales } from '@/redux/features/supermarketSales/supermarketSalesSlice'
+import { useAppDispatch } from '@/redux/hooks'
+import { useEffect } from 'react'
 
 export default function Home() {
-  const dispatch = useAppDispatch()
-  const supermarketData = useAppSelector(selectSupermarketSalesData)
+	const dispatch = useAppDispatch()
 
-  const [supermarketSalesData, setSupermarketSalesData] = useState<ISupermarketSales>()
-
-  useEffect(() => {
-    dispatch(getSupermarketSales())
-  }, [])
-  
-  console.log(supermarketData)
+	useEffect(() => {
+		dispatch(getSupermarketSales())
+	}, [])
 
 	return (
 		<main className='pt-5'>
-      <ChartGrid />
+			<ChartGrid />
 		</main>
 	)
 }
