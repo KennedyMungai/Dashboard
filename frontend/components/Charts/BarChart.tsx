28ff5148e72b5c93c6ee449cec2e8cf1d1850802
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 12:12:08 +0300

    Made some updated for the BarChart

--- frontend/components/Charts/BarChart.tsx
+++ frontend/components/Charts/BarChart.tsx
@@ -1,12 +1,17 @@
+import ChartCardTemplate from './ChartCardTemplate'
 
 type Props = {
-    data:total_sales_per_branch
+	data: total_sales_per_branch
+	title: string
+	description: string
 }
 
-const BarChart = ({data}: Props) => {
-  return (
-    <div>BarChart</div>
-  )
+const BarChart = ({ data, title, description }: Props) => {
+	return (
+		<ChartCardTemplate title={title} description={description}>
+			BarChart
+		</ChartCardTemplate>
+	)
 }
 
-export default BarChart 
+export default BarChart
