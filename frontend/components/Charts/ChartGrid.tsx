38abed91b162580ca56b805a2cf6ddc2473739a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 11:59:21 +0300

    Deleted the unused code

--- frontend/components/Charts/ChartGrid.tsx
+++ frontend/components/Charts/ChartGrid.tsx
@@ -1,78 +1,17 @@
 'use client'
 
-import { Line } from 'react-chartjs-2'
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle
-} from '../ui/card'
-import { useAppSelector } from '@/redux/hooks'
 import { selectSupermarketSalesData } from '@/redux/features/supermarketSales/supermarketSalesSlice'
-import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend
-} from 'chart.js'
+import { useAppSelector } from '@/redux/hooks'
+import GrossProfit from './GrossProfit'
 
 type Props = {}
-
-ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend
-)
 
 const ChartGrid = (props: Props) => {
 	const salesData = useAppSelector(selectSupermarketSalesData)
 
-	const data = salesData.gross_income_data
-
 	return (
 		<div className='w-full h-full p-5 flex flex-1 flex-wrap'>
-				<Card className='w-[20rem]'>
-					<CardHeader>
-						<CardTitle>Line Chart</CardTitle>
-						<CardDescription>
-							Line Chart Data for the supermarket data
-						</CardDescription>
-					</CardHeader>
-					<CardContent className='flex m-2 p-2'>
-						<Line
-							datasetIdKey='id'
-							data={{
-								labels: data.labels,
-								datasets: [
-									{
-										label: data.label,
-										data: data.data,
-										backgroundColor: [
-											'rgba(255, 99, 132, 0.2)',
-											'rgba(54, 162, 235, 0.2)',
-											'rgba(255, 206, 86, 0.2)'
-										],
-										borderColor: [
-											'rgba(255, 99, 132, 1)',
-											'rgba(54, 162, 235, 1)',
-											'rgba(255, 206, 86, 1)'
-										],
-										borderWidth: 1
-									}
-								]
-							}}
-						/>
-					</CardContent>
-				</Card>
+			<GrossProfit data={salesData.gross_income_data} />	
 		</div>
 	)
 }
